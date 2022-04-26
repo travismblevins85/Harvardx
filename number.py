a7bbb8a0fb2d242f6128988ae363102948b6b3b2
Travis Blevins <travismblevins85@gmail.com>
2022-04-26 19:25:30 -0400

    number

--- number.py
+++ number.py
@@ -1,10 +1,12 @@
-while True:
-    try:
-        x = int(input("What's x? - "))
-    except ValueError:
-        print("x is an integer")
-    else:
-        break
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-print(f"x is {x}")    
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? - "))
+        except ValueError:
+            pass     
 
+main()
